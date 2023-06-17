62ef4eec9b14fad4e6d06a2d73e01859e0fd8a38
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-17 16:35:27 -0300

    server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import fastify from "fastify";
+import cors from '@fastify/cors'
+
+import { userRoutes } from "./routes/user";
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true
+})
+
+app.register(userRoutes)
 
 app
   .listen({ port: 3333 })
