8959f128a2beeba460df7f152a1d931027062a54
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-18 08:21:27 -0300

    auth-routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,34 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { compare } from "bcryptjs";
 
-export function authRoutes(app: FastifyInstance){
-  app.post('/register', async (request) => {
+export async function authRoutes(app: FastifyInstance){
+  app.post('/register', async (request, response) => {
+    const userSchema = z.object({
+      email: z.string(),
+      password: z.string()
+    })
+
+    const userInfo = userSchema.parse(request.body)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        email: userInfo.email
+      }
+    })
+
+    if(!user){
+      return response.send({ message: "Usuário não encontrado..." })
+    }
+
+    const token = app.jwt.sign({
+      id: user.id
+    }, {
+      expiresIn: '5 days'
+    })
+
+    return { token }
 
   })
 }
